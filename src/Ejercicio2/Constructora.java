2ba6e81fc404016caa0ded3bec6bff025880272b
Fabrizzio2k1 <fabrizzio.federico@davinci.edu.ar>
2024-09-14 19:42:38 -0300

    Tengo que arreglar el validarNumero.

--- src/Ejercicio2/Constructora.java
+++ src/Ejercicio2/Constructora.java
@@ -2,6 +2,8 @@ package Ejercicio2;
 
 import java.time.LocalDate;
 
+import javax.swing.JOptionPane;
+
 public class Constructora {
 	private String nombre;
 	private String ubicacion;
@@ -65,6 +67,45 @@ public class Constructora {
 
 	public void setFechaProgreso(LocalDate fechaProgreso) {
 		this.fechaProgreso = fechaProgreso;
+	}
+	
+	public void crearProyecto() {
+		this.nombre = validarCaracteres("Ingrese el nombre del proyecto");
+		this.ubicacion = validarCaracteres("Ingrese la ubicación del proyecto");
+		this.cliente = validarCaracteres("Ingrese el cliente del proyecto");
+		this.fechaInicio = LocalDate.of(validarNumeros("Ingrese Año"),validarNumeros("Ingrese Mes"),validarNumeros("Ingrese Dia"));
+		this.fechaProgreso = this.fechaInicio;
+	}
+	
+	public String validarCaracteres(String mensaje) {
+		String palabra = "";
+		while (palabra.equals("")) {
+			palabra = JOptionPane.showInputDialog(mensaje);
+		}
+		return palabra;
+	}
+	
+	public int validarNumeros(String mensaje) {
+		boolean flag = false;
+		String num = "";
+		
+		while (!flag) {
+			num = JOptionPane.showInputDialog(mensaje);
+			for (int i = 0; i < num.length(); i++) {
+				if(!Character.isAlphabetic(num.charAt(i))) {
+					flag = true;
+					break;
+				}
+			}
+		}
+		
+		return Integer.parseInt(num);
+	}
+
+	@Override
+	public String toString() {
+		return "Constructora [nombre=" + nombre + ", ubicacion=" + ubicacion + ", cliente=" + cliente + ", fechaInicio="
+				+ fechaInicio + ", Etapas=" + Etapas + ", fechaProgreso=" + fechaProgreso + "]";
 	}
 	
 	
--- src/Ejercicio2/Main.java
+++ src/Ejercicio2/Main.java
@@ -1,10 +1,30 @@
 package Ejercicio2;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Constructora c1 = new Constructora();
+		String menu [] = {"Crear Proyecto", "Evaluar Etapas", "Ver Etapas", "Revisar Proyecto" , "Salir"};
+		int opcion;
+		
+		do {
+			opcion = JOptionPane.showOptionDialog(null, "Elija una Opción", null, 0, 0, null, menu, menu[0]);
+			switch (opcion) {
+			case 0: {
+				c1.crearProyecto();
+				break;
+			}
+			case 3: {
+				JOptionPane.showMessageDialog(null, c1);
+				break;
+			}
+			default:
+			}
+		} while (opcion!=4);
+		
 	}
 
 }
